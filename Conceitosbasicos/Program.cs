ef382b6582e0db3238df6c123671fcf5fbe62720
2022101873 <2022101873@LAB2-003>
2023-08-22 20:14:36 -0300

    Adicionar arquivos de projeto.

---
+++ Conceitosbasicos/Program.cs
@@ -0,0 +1,18 @@
+﻿/*using Conceitosbasicos;
+
+int num1 = 20;
+int num2 = 11, result;
+
+Somador soma;
+soma = new Somador();
+
+result = soma.Soma(num1, num2);
+
+Console.WriteLine(result);*/
+/*
+ * using Conceitosbasicos;
+ * Console.WriteLine(Conversor.MetrosMilimetros(1.85);
+    */
+using Conceitosbasicos;
+double metros = 1.75 ;
+Console.WriteLine($"A medida {metros} metros corresponde a {Conversor.MetrosMilimetros(metros)} milimetros");
