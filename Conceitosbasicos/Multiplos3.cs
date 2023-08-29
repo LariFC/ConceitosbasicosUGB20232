3ab1df6d2c1e0c3ec798929783cbb84da79f456d
2022101873 <larissafcarius2@gmail.com>
2023-08-29 19:27:39 -0300

    alterado

---
+++ Conceitosbasicos/Multiplos3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conceitosbasicos
+{
+    public class Multiplos3
+    {
+        public static bool Multiplos(double numeros)
+        { 
+            return numeros % 3 == 0;
+        }
+    }
+}
--- Conceitosbasicos/Program.cs
+++ Conceitosbasicos/Program.cs
@@ -1,18 +1,38 @@
-﻿/*using Conceitosbasicos;
+﻿/*
+using Conceitosbasicos;
 
 int num1 = 20;
 int num2 = 11, result;
 
 Somador soma;
 soma = new Somador();
-
 result = soma.Soma(num1, num2);
 
-Console.WriteLine(result);*/
-/*
- * using Conceitosbasicos;
- * Console.WriteLine(Conversor.MetrosMilimetros(1.85);
-    */
-using Conceitosbasicos;
+Console.WriteLine(result);
+
+-----------------------------------------------------------------------
+Console.WriteLine(Conversor.MetrosMilimetros(1.85);
 double metros = 1.75 ;
 Console.WriteLine($"A medida {metros} metros corresponde a {Conversor.MetrosMilimetros(metros)} milimetros");
+*/
+
+using Conceitosbasicos;
+int Cont = 1;
+
+while (Cont < 100)
+{
+    if( Multiplos3.Multiplos(Cont) == true)
+        {
+            Console.WriteLine(Cont);
+        }
+    else
+        {
+
+        }
+    Cont++;
+}
+
+
+
+
+
